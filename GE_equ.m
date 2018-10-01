7d15e291ae7df7ab561a3f0d4c934f041a237731
Welegend <wzhwc@126.com>
2018-10-01 16:55:52 +0800

    添加了一个列主元高斯消去法解方程组

---
+++ GE_equ.m
@@ -0,0 +1,38 @@
+% 函数功能：实现Ax=b的列主元高斯消去法（Gauss Elimination），其中A为n阶方阵
+% 输入：矩阵A，b
+% 输出：得到的解矩阵x
+% 使用范围：列主元高斯消去法方法稳定，计算量小，适用于求解良态方程组
+% 顺序主子式均不等于0，对称正定矩阵，严格对角占优矩阵时可以使用
+
+function x = GE_equ(A, b)
+%%  列主元高斯消去法，变换矩阵
+Ab = [A, b];
+[~, n] = size(A);
+for k = 1: n - 1
+     % 列主元，找到第k列最大值的那一行，并交换到对角线上
+    [~, w] = max(Ab(k: n, k));
+    Ab([k, w + k - 1], :) = Ab([w + k - 1, k], :);
+    
+     % 作用是用第k行的值往下消
+    Ab(k + 1: n, k) = Ab(k + 1: n, k) / Ab(k, k);
+    Ab(k + 1: n, k + 1: end) = Ab(k + 1: n, k + 1: end) - Ab(k + 1: n, k) * Ab(k, k + 1: end);
+end
+
+A = Ab(:, 1: size(A, 2));
+b = Ab(:, end - size(b, 2) + 1: end);
+
+%% 以下是R为方阵且上三角矩阵时，求解方程组
+x_temp = zeros(n, size(b, 2)); % 预设x解向量
+x = zeros(n, size(b, 2));
+
+for k = 1: n
+    x_temp(end, :) = b(end, :) ./ A(end, end); % 先算第n个x
+    b = b - A(:, end) * x_temp(end, :); % 把计算出来的x解代入方程，简化方程
+    x(n - k + 1, :) = x_temp(n - k + 1, :);
+    x_temp(end, :) = [];
+    A(:, end) = [];
+    A(end, :) = [];
+    b(end, :) = [];
+end
+
+end
--- Household_equ.m
+++ Household_equ.m
@@ -1,16 +1,19 @@
 % 函数功能：解方程组Ax=b，Household QR分解解法，此方程解法只适用于A列满秩的情况
 % 输入：矩阵A、b
 % 输出：解向量x
+% 使用范围：Household变换的QR分解计算方程组十分稳定，常用于计算病态方程组，但计算量约是高斯消去法的2倍
 
 function x = Household_equ(A, b)
 
-[Q, R] = Household_QR([A, b]);
+[~, R] = Household_QR([A, b]);
 y = R(:, end - size(b, 2) + 1: end); % 考虑到b可能不止一列的情况
 R(:, end - size(b, 2) + 1: end) = []; % 转化为求解R * x = y，R为上三角矩阵
 
-[m, n] = size(R);
+[~, n] = size(R);
 R = R(1: n, :); % 在m>n的情况下，把R矩阵变成方阵
 y = y(1: n, :); % R变成方阵，y随之变化
+
+%% 以下是R为方阵且上三角矩阵时，求解方程组
 x_temp = zeros(n, size(y, 2)); % 预设x解向量
 x = zeros(n, size(y, 2));
 
