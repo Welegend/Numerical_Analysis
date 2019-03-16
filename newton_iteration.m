5b27bb9123f5ad93e2030312cfc0499125fbde4b
geniusjoe <843520313@qq.com>
2019-03-16 19:41:57 +0800

    change disp() function into fprintf function

--- newton_iteration.m
+++ newton_iteration.m
@@ -5,7 +5,7 @@ function res=newton_iteration(first_x_value,fx)
 % 
 % can change precision and max_iteration_times and min_diff_value at the following
 precision=1e-6;
-max_iteration_times=10;
+max_iteration_times=20;
 min_diff_value=1e-4;
 % ------------------------------------------------------------%
 syms x;
@@ -18,17 +18,16 @@ x_next=double(subs(fx_next,x,x_before));
 iterate_times=1;
 while (abs(x_next-x_before)>precision) && (iterate_times<=max_iteration_times)
     % if f'x < min_diff_value,
-    % throw exception
+    % throw warning
     if abs(subs(diff_fx,x,x_before))<min_diff_value
-    	disp('\ndiff less than min_diff_value\n');
-    	disp('min diff value is\n',double(abs(subs(diff_fx,x,x_before))));
-    	break;
+    	fprintf('\n diff less than min_diff_value \n min diff value is %06f\n', ...
+    			double(abs(subs(diff_fx,x,x_before))));
+        break;
     end
     
     % print each iterate x_next value
-    current_display=[x_next;iterate_times];
-    formatSpec = '\nthe current x_value is %06f,\nthe current iterate_times is %d\n';
-    fprintf(formatSpec,current_display);
+    fprintf('\nthe current x_value is %06f,\nthe current iterate_times is %d\n', ...
+   			x_next,iterate_times);
     
     % next iterate
     x_before=x_next;
