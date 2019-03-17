8fe9d895de32b33209826c8dbba810f6975b298a
geniusjoe <843520313@qq.com>
2019-03-17 13:20:59 +0800

    Update newton_iteration.m

--- newton_iteration.m
+++ newton_iteration.m
@@ -1,5 +1,5 @@
-function res=newton_iteration(first_x_value,fx)
-% \param first_x_value: start position
+function res=newton_iteration(x_value_list,fx)
+% \param x_value_list: list of start position
 % \param fx: syms expression
 % \return res: the root value of fx
 %
@@ -9,40 +9,47 @@ max_iteration_times=20;
 min_diff_value=1e-4;
 % ------------------------------------------------------------%
 % example:
-% >> syms x;
-% fx=x-exp(-x);
-% newton_iteration(0.5,fx);
+% syms x;
+% fx=x^2-2*x*exp(-x)+exp(-2*x);
+% newton_iteration([0.5 0.6 0.7],fx);
 %
 % return
-% the current x_value is 0.567143,
-% the current iterate_times is 2
+% the answer is
+%     0.5671
+%     0.5672
+%     0.5672
 % ------------------------------------------------------------%
 
 syms x;
 diff_fx=diff(fx);
 fx_next=x-fx/diff_fx;
-
-x_before=double(first_x_value);
-x_next=double(subs(fx_next,x,x_before));
-
-iterate_times=1;
-while (abs(x_next-x_before)>precision) && (iterate_times<=max_iteration_times)
-    % if f'x < min_diff_value,
-    % throw warning
-    if abs(subs(diff_fx,x,x_before))<min_diff_value
-        fprintf('\n diff less than min_diff_value \n min diff value is %06f\n', ...
-            double(abs(subs(diff_fx,x,x_before))));
-        break;
-    end
-    
-    % print each iterate x_next value
-    fprintf('\nthe current x_value is %06f,\nthe current iterate_times is %d\n', ...
-        x_next,iterate_times);
-    
-    % next iterate
-    x_before=x_next;
+res=zeros(1,size(x_value_list,2));
+for i = 1:size(x_value_list,2)
+    first_x_value=x_value_list(i);
+    x_before=double(first_x_value);
     x_next=double(subs(fx_next,x,x_before));
-    iterate_times=iterate_times+1;
+    
+    iterate_times=1;
+    while (abs(x_next-x_before)>precision) && (iterate_times<=max_iteration_times)
+        % if f'x < min_diff_value,
+        % return
+        if abs(subs(diff_fx,x,x_before))<min_diff_value
+            fprintf('\n diff less than min_diff_value \n min diff value is %06f\n', ...
+                double(abs(subs(diff_fx,x,x_before))));
+            break;
+        end
+        
+        % print each iterate x_next value
+        fprintf('\nthe current x_value is %06f,\nthe current iterate_times is %d\n', ...
+            x_next,iterate_times);
+        
+        % next iterate
+        x_before=x_next;
+        x_next=double(subs(fx_next,x,x_before));
+        iterate_times=iterate_times+1;
+    end
+    res(i)=x_next;
 end
-res=x_next;
+fprintf('\nthe answer is\n');
+disp(transpose(res));
 end
